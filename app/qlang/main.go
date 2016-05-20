52fe79c4b2d8fa7b3702097bd97acaed130e9438
damon.chen <netubu@gmail.com>
2016-05-20 09:14:10 +0800

    add readline support for qlang

--- app/qlang/main.go
+++ app/qlang/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io"
 	"io/ioutil"
-	"strings"
 	"os"
+	"strings"
 
+	qipt "qlang.io/qlang.v2/interpreter"
 	"qlang.io/qlang.v2/qlang"
 	qall "qlang.io/qlang/qlang.all"
-	qipt "qlang.io/qlang.v2/interpreter"
+
+	"gopkg.in/readline.v1"
 )
 
 // -----------------------------------------------------------------------------
@@ -60,21 +62,50 @@ func main() {
 	}
 	lang.SetLibs(libs)
 
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		line := strings.Trim(scanner.Text(), " \t\r\n")
+	historyFile := os.Getenv("HOME") + "/.qlang.history"
+
+	rl, err := readline.NewEx(&readline.Config{
+		Prompt:          "qlang> ",
+		HistoryFile:     historyFile,
+		InterruptPrompt: "^C",
+		EOFPrompt:       "exit",
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer rl.Close()
+
+	for {
+		line, err := rl.Readline()
+		if err != nil {
+			if err == readline.ErrInterrupt {
+				if len(line) == 0 {
+					break
+				} else {
+					continue
+				}
+			} else if err == io.EOF {
+				break
+			}
+
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+
+		line = strings.TrimSpace(line)
 		if line == "" {
 			continue
 		}
+
 		ret = nil
-		err := lang.SafeEval(line)
+		err = lang.SafeEval(line)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			continue
 		}
-		fmt.Printf("> %v\n\n", ret)
+
+		fmt.Printf("=> %v\n", ret)
 	}
 }
 
 // -----------------------------------------------------------------------------
-
