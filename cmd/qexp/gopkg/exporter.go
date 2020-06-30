ac923f15aa1123699d283baa3dbc55c7f4c67eaa
visualfc <visualfc@gmail.com>
2020-07-01 07:21:05 +0800

    cmd/qexp/gopkg: spec.ConstUnboundInt and check int64 or uint64

--- cmd/qexp/gopkg/exporter.go
+++ cmd/qexp/gopkg/exporter.go
@@ -325,13 +325,9 @@ $argInit	$retAssign$fn($args)
 	return nil
 }
 
-const (
-	specName = "spec"
-)
-
 // ExportConst exports a go consts.
 func (p *Exporter) ExportConst(typ *types.Const) error {
-	kind, err := constKind(typ, specName)
+	kind, err := constKind(typ, "spec")
 	if err != nil {
 		return err
 	}
@@ -346,15 +342,15 @@ func (p *Exporter) ExportConst(typ *types.Const) error {
 	c.name = typ.Name()
 	c.kind = kind
 	c.val = fullName
-	if typ.Val().Kind() == constant.Int {
+	if kind == "spec.ConstUnboundInt" && typ.Val().Kind() == constant.Int {
 		value := typ.Val().String()
 		_, err := strconv.ParseInt(value, 10, 32)
 		if err != nil {
 			if value[0] == '-' {
-				c.kind = specName + ".Int64"
+				c.kind = "spec.Int64"
 				c.val = "int64(" + fullName + ")"
 			} else {
-				c.kind = specName + ".Uint64"
+				c.kind = "spec.Uint64"
 				c.val = "uint64(" + fullName + ")"
 			}
 		}
