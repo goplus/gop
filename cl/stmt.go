6a44e6483047599cedd6241d3162a5f9adb72a73
JessonChan <chinachendongpo@gmail.com>
2020-07-05 22:23:53 +0800

    add one improvement todo

--- cl/stmt.go
+++ cl/stmt.go
@@ -214,7 +214,8 @@ func compileBranchStmt(ctx *blockCtx, v *ast.BranchStmt) {
 func compileLabeledStmt(ctx *blockCtx, v *ast.LabeledStmt) {
 	label := ctx.defineLabel(v.Label.Name)
 	// make sure all labels in golang code  will be used
-	// ctx.out.Jmp(label)
+	// TODO improvement exec/bytecode not to jump if delta==0
+	ctx.out.Jmp(label)
 	ctx.out.Label(label)
 	ctx.currentLabel = v
 	compileStmt(ctx, v.Stmt)
