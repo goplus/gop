0a5382fcd537af69e6c1d92e52cb94d89a60ee7d
xushiwei <x@goplus.org>
2020-07-18 09:37:34 +0800

    gop/ast/gopq

--- exec/bytecode/testdata_test.go
+++ exec/bytecode/testdata_test.go
@@ -27,7 +27,7 @@ import (
 // -----------------------------------------------------------------------------
 
 func TestFromTestdata(t *testing.T) {
-	sel, exclude := "", "23"
+	sel, exclude := "", ""
 	dir, err := os.Getwd()
 	if err != nil {
 		t.Fatal("Getwd failed:", err)
