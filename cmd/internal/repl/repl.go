2b5503934931372802c3a22a9c9fcdf541341d76
jiangzhi <jiangzhi@qiniu.com>
2020-07-26 01:01:37 +0800

    repl supported

--- cmd/internal/repl/repl.go
+++ cmd/internal/repl/repl.go
@@ -1,3 +1,20 @@
+/*
+ Copyright 2020 The GoPlus Authors (goplus.org)
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+// Package run implements the ``gop repl'' command.
 package repl
 
 import (
@@ -24,11 +41,12 @@ var Cmd = &base.Command{
 }
 
 type repl struct {
-	src          string
-	preContext   exec.Context
-	ip           int
-	prompt       string
-	continueMode bool
+	src          string       // the whole source code from repl
+	preContext   exec.Context // store the context after exec
+	ip           int          // store the ip after exec
+	prompt       string       // the prompt type in console
+	continueMode bool         // switch to control the promot type
+	liner        *liner.State
 }
 
 const (
@@ -40,10 +58,11 @@ const (
 func runCmd(cmd *base.Command, args []string) {
 	fmt.Println(welcome)
 	liner := liner.NewLiner()
-	defer liner.Close()
-	rInstance := repl{prompt: standardPrompt}
+	rInstance := repl{prompt: standardPrompt, liner: liner}
+	defer rInstance.liner.Close()
+
 	for {
-		l, err := liner.Prompt(string(rInstance.prompt))
+		l, err := rInstance.liner.Prompt(string(rInstance.prompt))
 		if err != nil {
 			if err == io.EOF {
 				fmt.Printf("\n")
@@ -52,35 +71,49 @@ func runCmd(cmd *base.Command, args []string) {
 			fmt.Printf("Problem reading line: %v\n", err)
 			continue
 		}
-		if l != "" {
-			// mainly for scroll up
-			liner.AppendHistory(l)
-		}
-		if rInstance.continueMode {
-			if l != "" {
-				rInstance.src += string(l) + "\n"
-				continue
-			}
-			// input nothing means jump out continue mode
-			rInstance.continueMode = false
-			rInstance.prompt = standardPrompt
-		}
-
-		if nil == rInstance.run(string(l)) {
-			rInstance.src += string(l) + "\n"
-		}
+		rInstance.replOneLine(l)
 	}
+}
+
+// replOneLine handle one line
+func (r *repl) replOneLine(line string) {
+	if line != "" {
+		// mainly for scroll up
+		r.liner.AppendHistory(line)
+	}
+	canRun := true
+	if r.continueMode {
+		canRun = r.continueModeByLine(line)
+	}
+	if canRun {
+		r.run(line)
+	}
+}
+
+// handleContinueMode check if continue-mode should continue :)
+func (r *repl) continueModeByLine(line string) (canRun bool) {
+	if line != "" {
+		r.src += line + "\n"
+		return false
+	}
+	// input nothing means jump out continue mode
+	r.continueMode = false
+	r.prompt = standardPrompt
+	return true
 }
 
 // run execute the input line
 func (r *repl) run(newLine string) (err error) {
 	src := r.src + newLine + "\n"
 	defer func() {
+		if err == nil {
+			r.src = src
+		}
 		if errR := recover(); errR != nil {
 			replErr(newLine, errR)
 			err = errors.New("panic err")
 			// TODO: Need a better way to log and show the stack when crash
-			// It is too long to print stack on terminal even only print part of the them:
+			// It is too long to print stack on terminal even only print part of the them; not friendly to user
 		}
 	}()
 	fset := token.NewFileSet()
@@ -113,13 +146,16 @@ func (r *repl) run(newLine string) (err error) {
 	ctx := exec.NewContext(code)
 	if r.ip != 0 {
 		// not first time, restore pre var
-		r.preContext.CloneSetterScope(ctx)
+		r.preContext.CloneSetVarScope(ctx)
 	}
 	currentIp := ctx.Exec(r.ip, code.Len())
 	r.preContext = *ctx
+	// currentip - 1 iif t := false; t {s the index of `return`
+	// next time it will replace by new code from newLine
 	r.ip = currentIp - 1
 	return
 }
+
 func replErr(line string, err interface{}) {
 	fmt.Println("code run fail : ", line)
 	fmt.Println("repl err: ", err)
