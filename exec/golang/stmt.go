81f907b0c3dfd70a1fee68ac64dcd118499a9b60
visualfc <visualfc@gmail.com>
2020-08-11 21:56:20 +0800

    golang: fix JmpIf

--- exec/golang/stmt.go
+++ exec/golang/stmt.go
@@ -89,7 +89,12 @@ func GotoIf(p *Builder, cond ast.Expr, l *Label) *ast.IfStmt {
 
 // JmpIf instr
 func (p *Builder) JmpIf(jc exec.JmpFlag, l *Label) *Builder {
-	cond := p.rhs.Pop().(ast.Expr)
+	var cond ast.Expr
+	if jc.NotPop() {
+		cond = p.rhs.Get(-1).(ast.Expr)
+	} else {
+		cond = p.rhs.Pop().(ast.Expr)
+	}
 	switch jc.Cond() {
 	case exec.JcFalse:
 		cond = &ast.UnaryExpr{Op: token.NOT, X: cond}
