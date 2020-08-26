10bbfe04496db4fd5dfbf4f7feb8277e58e14f61
visualfc <visualfc@gmail.com>
2020-08-26 20:07:20 +0800

    cl: fix compileTypeCast

--- cl/goinstr.go
+++ cl/goinstr.go
@@ -337,8 +337,7 @@ func compileTypeCast(typ reflect.Type, ctx *blockCtx, v *ast.CallExpr) func() {
 			panicExprNotValue(n)
 		}
 		tIn := iv.Type()
-		typ = reflectx.ToType(typ)
-		if !tIn.ConvertibleTo(typ) {
+		if !tIn.ConvertibleTo(reflectx.ToType(typ)) {
 			log.Panicf("compileTypeCast: can't convert type `%v` to `%v`\n", tIn, typ)
 		}
 		ctx.out.TypeCast(tIn, typ)
