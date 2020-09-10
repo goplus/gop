8a9646ff8f7a87a13cc3fb6418b2d4404c37aa03
visualfc <visualfc@gmail.com>
2020-09-10 16:33:40 +0800

    exec/bytecode: fix #586 user-defined struct unexport field use unsafe.Pointer

--- exec/bytecode/struct.go
+++ exec/bytecode/struct.go
@@ -158,8 +158,7 @@ func (p *Builder) Struct(typ reflect.Type, arity int) *Builder {
 }
 
 func execStruct(i Instr, p *Context) {
-	typ := getType(i&bitsOpCallFuncvOperand, p)
-	typStruct := toType(typ)
+	typStruct := getType(i&bitsOpCallFuncvOperand, p)
 	arity := int((i >> bitsOpCallFuncvShift) & bitsFuncvArityOperand)
 	if arity == bitsFuncvArityMax {
 		arity = p.Pop().(int) + bitsFuncvArityMax
@@ -179,7 +178,7 @@ func makeStruct(typStruct reflect.Type, arity int, p *Context) {
 	v := reflect.New(typStruct).Elem()
 	for i := 0; i < n; i += 2 {
 		index := args[i].(int)
-		v.Field(index).Set(reflect.ValueOf(args[i+1]))
+		Field(v, index).Set(reflect.ValueOf(args[i+1]))
 	}
 	if ptr {
 		p.Ret(n, v.Addr().Interface())
