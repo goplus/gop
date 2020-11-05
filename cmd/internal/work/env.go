522316090698352df4950071007c27f291dc8341
visualfc <visualfc@gmail.com>
2020-11-05 23:55:47 +0800

    x

--- cmd/internal/work/env.go
+++ cmd/internal/work/env.go
@@ -5,6 +5,13 @@ import (
 	"path/filepath"
 )
 
+func GOPROOT() string {
+	if gopbin := os.Getenv("GOPROOT"); gopbin != "" {
+		return gopbin
+	}
+	return filepath.Join(os.Getenv("HOME"), "gop")
+}
+
 func GOPBIN() string {
 	if gopbin := os.Getenv("GOPBIN"); gopbin != "" {
 		return gopbin
