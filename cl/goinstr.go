963675202a48852894c4b86e845b192b3e0f51c6
visualfc <visualfc@gmail.com>
2020-11-28 08:56:32 +0800

    fix const type cast

--- cl/goinstr.go
+++ cl/goinstr.go
@@ -23,6 +23,7 @@ import (
 	"github.com/goplus/gop/ast"
 	"github.com/goplus/gop/ast/spec"
 	"github.com/goplus/gop/exec.spec"
+	"github.com/goplus/reflectx"
 )
 
 // -----------------------------------------------------------------------------
@@ -403,8 +404,12 @@ func compileTypeCast(typ reflect.Type, ctx *blockCtx, v *ast.CallExpr) func() {
 	if kind <= reflect.Complex128 || kind == reflect.String { // can be constant
 		if cons, ok := in.(*constVal); ok {
 			cons.kind = typ.Kind()
-			if typ.PkgPath() != "" {
+			if reflectx.IsNamedType(typ) {
 				ctx.infer.Ret(1, &goValue{typ})
+				return func() {
+					pushConstVal(ctx.out, cons)
+					ctx.out.TypeCast(cons.Type(), typ)
+				}
 			}
 			return func() {
 				pushConstVal(ctx.out, cons)
