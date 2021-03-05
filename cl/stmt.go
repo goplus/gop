c68291bfb500652230643081f498f9c77c37e913
visualfc <visualfc@gmail.com>
2021-03-05 23:26:08 +0800

    cl: compileAssignStmt reset indirect

--- cl/stmt.go
+++ cl/stmt.go
@@ -651,6 +651,7 @@ func compileAssignStmt(ctx *blockCtx, expr *ast.AssignStmt) {
 	}
 	count := len(expr.Lhs)
 	ctx.underscore = 0
+	ctx.indirect = 0
 	for i := len(expr.Lhs) - 1; i >= 0; i-- {
 		compileExprLHS(ctx, expr.Lhs[i], expr.Tok)
 	}
