7f615eb876f551642fb5b66b989d1905fb2ec829
visualfc <visualfc@gmail.com>
2021-04-10 21:11:14 +0800

    cl: add TestTypeAssertCheck

--- cl/expr_test.go
+++ cl/expr_test.go
@@ -2418,3 +2418,120 @@ func TestTypAssertImpossible(t *testing.T) {
 	test(buf)
 	`, "", "impossible type assertion: int does not implement main.Stringer")
 }
+
+var typeAssertCheck = `
+package main
+
+import "os"
+
+var (
+	Bool   int = 0
+	Int    int = 1
+	Float  int = 2
+	String int = 3
+	Struct int = 4
+	Chan   int = 5
+	Array  int = 6
+	Map    int = 7
+	Func   int = 8
+	Last   int = 9
+)
+
+type S struct {
+	a int
+}
+
+var s S = S{1234}
+
+var c = make(chan int)
+
+var a = []int{0, 1, 2, 3}
+
+var m = make(map[string]int)
+
+func assert(b bool, s string) {
+	if !b {
+		println(s)
+		os.Exit(1)
+	}
+}
+
+func f(i int) interface{} {
+	switch i {
+	case Bool:
+		return true
+	case Int:
+		return 7
+	case Float:
+		return 7.4
+	case String:
+		return "hello"
+	case Struct:
+		return s
+	case Chan:
+		return c
+	case Array:
+		return a
+	case Map:
+		return m
+	case Func:
+		return f
+	}
+	panic("bad type number")
+}
+
+func main() {
+	for i := Bool; i < Last; i++ {
+		switch x := f(i).(type) {
+		case bool:
+		//	assert(x == true && i == Bool, "bool")
+		case int:
+			assert(x == 7 && i == Int, "int")
+		case float64:
+			assert(x == 7.4 && i == Float, "float64")
+		case string:
+			assert(x == "hello" && i == String, "string")
+		case S:
+			assert(x.a == 1234 && i == Struct, "struct")
+		case chan int:
+			assert(x == c && i == Chan, "chan")
+		case []int:
+			assert(x[3] == 3 && i == Array, "array")
+		case map[string]int:
+			assert(x != nil && i == Map, "map")
+		case func(i int) interface{}:
+			assert(x != nil && i == Func, "fun")
+		default:
+			assert(false, "unknown")
+		}
+	}
+
+	// boolean switch (has had bugs in past; worth writing down)
+	switch {
+	case true:
+		assert(true, "switch 2 bool")
+	default:
+		assert(false, "switch 2 unknown")
+	}
+
+	switch true {
+	case true:
+		assert(true, "switch 3 bool")
+	default:
+		assert(false, "switch 3 unknown")
+	}
+
+	switch false {
+	case false:
+		assert(true, "switch 4 bool")
+	default:
+		assert(false, "switch 4 unknown")
+	}
+
+	println("hello")
+}
+`
+
+func TestTypeAssertCheck(t *testing.T) {
+	cltest.Expect(t, typeAssertCheck, "hello\n")
+}
