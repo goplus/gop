4bb5006bc79c1cbdef2751b965fcd1a03f623a92
visualfc <visualfc@gmail.com>
2021-04-15 23:15:23 +0800

    cl: compileTypeCase check user type

--- cl/goinstr.go
+++ cl/goinstr.go
@@ -23,7 +23,6 @@ import (
 	"github.com/goplus/gop/ast"
 	"github.com/goplus/gop/ast/spec"
 	"github.com/goplus/gop/exec.spec"
-	"github.com/goplus/reflectx"
 )
 
 // -----------------------------------------------------------------------------
@@ -438,7 +437,7 @@ func compileTypeCast(typ reflect.Type, ctx *blockCtx, v *ast.CallExpr) func() {
 	if kind <= reflect.Complex128 || kind == reflect.String { // can be constant
 		if cons, ok := in.(*constVal); ok {
 			cons.kind = typ.Kind()
-			if reflectx.IsNamed(typ) {
+			if typ.PkgPath() != "" {
 				ctx.infer.Ret(1, &goValue{typ})
 				return func() {
 					pushConstVal(ctx.out, cons)
