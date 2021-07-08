aa1b93d674e9543094938669990b3543b5568b3d
xushiwei <x@goplus.org>
2021-07-08 23:47:13 +0800

    tutorial

---
+++ tutorial/14-Using-goplus-in-Go/gomain/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/goplus/gop/tutorial/14-Using-goplus-in-Go/foo"
+)
+
+func main() {
+	rmap := foo.ReverseMap(map[string]int{"Hi": 1, "Hello": 2})
+	fmt.Println(rmap)
+}
