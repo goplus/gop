dfeddfe4f607fec68f67a058510297b16bc9f16e
xushiwei <x@goplus.org>
2021-10-07 12:50:58 +0800

    v1.0.12

--- version.go
+++ version.go
@@ -19,7 +19,7 @@ package gop
 // buildVersion is the GoPlus tree's version string at build time.
 // This is set by the linker.
 var (
-	buildVersion string = "v1.0.11"
+	buildVersion string = "v1.0.12"
 )
 
 // Version returns the GoPlus tree's version string.
