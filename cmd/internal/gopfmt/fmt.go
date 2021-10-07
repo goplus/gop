00218e2a4d6937ad01ead6d49eeaec4d9d27ad66
wuxinyi <wuxinyi@qiniu.com>
2021-10-07 21:27:46 +0800

    support fmt from stdin

--- cmd/internal/gopfmt/fmt.go
+++ cmd/internal/gopfmt/fmt.go
@@ -20,6 +20,7 @@ package gopfmt
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"io/fs"
 	"io/ioutil"
 	"os"
@@ -92,6 +93,22 @@ func writeFileWithBackup(path string, target []byte) (err error) {
 	return os.Rename(tmpfile, path)
 }
 
+func processFile(filename string, in io.Reader, out io.Writer) error {
+	src, err := ioutil.ReadAll(in)
+	if err != nil {
+		return err
+	}
+
+	res, err := format.Source(src)
+	if err != nil {
+		return err
+	}
+
+	_, err = out.Write(res)
+
+	return err
+}
+
 func walk(path string, d fs.DirEntry, err error) error {
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
@@ -117,7 +134,10 @@ func runCmd(cmd *base.Command, args []string) {
 	flag.Parse(args)
 	narg := flag.NArg()
 	if narg < 1 {
-		cmd.Usage(os.Stderr)
+		if err := processFile("<standard input>", os.Stdin, os.Stdout); err != nil {
+			fmt.Println(err)
+			os.Exit(2)
+		}
 	}
 	for i := 0; i < narg; i++ {
 		path := flag.Arg(i)
