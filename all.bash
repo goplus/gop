ab427c71bf29ac3a8e4192e5caaf0daed22049f9
jichangjun <jinsdu@outlook.com>
2021-10-16 17:49:38 +0800

    feat: provide helps when gop command not found after installation

--- all.bash
+++ all.bash
@@ -9,4 +9,9 @@ if [ ! -e "$HOME/gop" ]; then
         ln -s $goproot $HOME/gop
     fi
 fi
-gop install ./...   # build all Go+ tutorials
+
+if  ! command -v gop &> /dev/null ; then
+    echo "gop has been installed. To use it conveniently, you need put $GOPATH/bin or $HOME/go/bin directory into your PATH environment variable, see \`go help install\`"
+else
+    gop install ./...   # build all Go+ tutorials
+fi
