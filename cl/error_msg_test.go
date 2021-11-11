ed7959a86eeddca1df4cae9d4197c720348c8bf4
visualfc <visualfc@gmail.com>
2021-11-11 23:06:58 +0800

    cl: add TestErrPosAdjust

--- cl/error_msg_test.go
+++ cl/error_msg_test.go
@@ -159,6 +159,27 @@ func foo(v map[int]bar) {
 `)
 }
 
+func TestErrPosAdjust(t *testing.T) {
+	codeErrorTest(t,
+		"./bar.gop:2:2: undefined: println1", `func main() {
+	println1 "hello"
+}
+`)
+	codeErrorTest(t,
+		"./bar.gop:1:1: undefined: println1", `println1 "hello"`)
+
+	codeErrorTest(t,
+		"./bar.gop:2:2: undefined: println1", `
+	println1 "hello"
+`)
+
+	codeErrorTest(t,
+		"./bar.gop:2:2: undefined: println1", `package main
+	println1 "hello"
+`)
+
+}
+
 func TestErrImport(t *testing.T) {
 	codeErrorTest(t,
 		`./bar.gop:8:2: confliction: NewEncoding declared both in "encoding/base64" and "encoding/base32"`, `
