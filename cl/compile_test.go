bf31f528bc09f318824305f29a238ec5da51f0e4
JessonChan <chinachendongpo@gmail.com>
2021-12-02 10:46:27 +0800

    newRange test cases

--- cl/compile_test.go
+++ cl/compile_test.go
@@ -3356,6 +3356,32 @@ func main() {
 `)
 }
 
+func TestRangeExpr7(t *testing.T) {
+	gopClTest(t, `
+println [x for x <- 0:3:1]
+`, `package main
+
+import (
+	fmt "fmt"
+	builtin "github.com/goplus/gop/builtin"
+)
+
+func main() {
+	fmt.Println(func() (_gop_ret []int) {
+		for _gop_it := builtin.NewRange__0(0, 3, 1).Gop_Enum(); ; {
+			var _gop_ok bool
+			x, _gop_ok := _gop_it.Next()
+			if !_gop_ok {
+				break
+			}
+			_gop_ret = append(_gop_ret, x)
+		}
+		return
+	}())
+}
+`)
+}
+
 func TestGoFuncInstr(t *testing.T) {
 	gopClTest(t, `package main
 
