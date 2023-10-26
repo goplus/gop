17c4a9ab87eec6200b8f75f6152b5f4486bb6153
xushiwei <x@goplus.org>
2023-10-26 18:05:15 +0800

    TestErrAssign

--- cl/builtin_test.go
+++ cl/builtin_test.go
@@ -50,6 +50,24 @@ func TestErrMultiStarRecv(t *testing.T) {
 	})
 }
 
+func TestErrAssign(t *testing.T) {
+	defer func() {
+		if e := recover(); e == nil {
+			t.Fatal("TestErrAssign: no panic?")
+		}
+	}()
+	ctx := &blockCtx{}
+	compileAssignStmt(ctx, &ast.AssignStmt{
+		Tok: token.DEFINE,
+		Lhs: []ast.Expr{
+			&ast.SelectorExpr{
+				X:   ast.NewIdent("foo"),
+				Sel: ast.NewIdent("bar"),
+			},
+		},
+	})
+}
+
 func TestErrPanicToRecv(t *testing.T) {
 	ctx := &blockCtx{
 		tlookup: &typeParamLookup{
