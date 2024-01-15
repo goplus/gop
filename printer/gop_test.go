4fea8c4b9894b8b1e42385232419a67612b42118
xushiwei <x@goplus.org>
2024-01-15 12:25:05 +0800

    x

--- printer/gop_test.go
+++ printer/gop_test.go
@@ -179,7 +179,7 @@ func TestFromTestdata(t *testing.T) {
 }
 
 func TestFromParse(t *testing.T) {
-	sel := "overload2"
+	sel := ""
 	dir, err := os.Getwd()
 	if err != nil {
 		t.Fatal("Getwd failed:", err)
