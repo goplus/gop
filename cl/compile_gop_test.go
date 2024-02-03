30a1a1c7899671031fe874714d370791f58a265d
xushiwei <x@goplus.org>
2024-02-04 00:06:09 +0800

    TestTypeAsParamsFunc: support template recv method

--- cl/compile_gop_test.go
+++ cl/compile_gop_test.go
@@ -1091,17 +1091,48 @@ import (
 	"reflect"
 )
 
+type basetype interface {
+	int | string
+}
+
+func Gopx_Row__0[T basetype](name string) {
+}
+
+func Gopx_Row__1[Array any](v int) {
+}
+
 func Gopx_Col[T any](name string) {
 	fmt.Printf("%v: %s\n", reflect.TypeOf((*T)(nil)).Elem(), name)
 }
+
+type Table struct {
+}
+
+func Gopt_Table_Gopx_Col__0[T basetype](p *Table, name string) {
+}
+
+func Gopt_Table_Gopx_Col__1[Array any](p *Table, v int) {
+}
 `, `
+var tbl *Table
+
 col string, "name"
 col int, "age"
+
+row string, 100
+
+tbl.col string, "foo"
+tbl.col int, 100
 `, `package main
+
+var tbl *Table
 
 func main() {
 	Gopx_Col[string]("name")
 	Gopx_Col[int]("age")
+	Gopx_Row__1[string](100)
+	Gopt_Table_Gopx_Col__0[string](tbl, "foo")
+	Gopt_Table_Gopx_Col__1[int](tbl, 100)
 }
 `)
 }
