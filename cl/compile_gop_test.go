1ec924611c7756fe971b953e41afadcee6058a6f
luoliwoshang <2643523683@qq.com>
2024-04-01 17:13:10 +0800

    test:update the overload test case

--- cl/compile_gop_test.go
+++ cl/compile_gop_test.go
@@ -376,6 +376,46 @@ func main() {
 `)
 }
 
+func TestOverloadFunc3(t *testing.T) {
+	gopClTest(t, `
+func addInt(a, b int) int {
+	return a + b
+}
+
+func addFloat(a, b float64) float64 {
+	return a * b
+}
+
+func add = (
+	func (a,b string) string { return a + b }
+	addInt
+	addFloat
+)
+
+println add(100, 7)
+println add(1.2, 3.14)
+`, `package main
+
+import "fmt"
+
+const Gopo_add = ",addInt,addFloat"
+
+func add__0(a string, b string) string {
+	return a + b
+}
+func addInt(a int, b int) int {
+	return a + b
+}
+func addFloat(a float64, b float64) float64 {
+	return a * b
+}
+func main() {
+	fmt.Println(addInt(100, 7))
+	fmt.Println(addFloat(1.2, 3.14))
+}
+`)
+}
+
 func TestOverload(t *testing.T) {
 	gopClTest(t, `
 import "github.com/goplus/gop/cl/internal/overload/foo"
