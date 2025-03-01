bd534ac2451e90f1bbcd286d2d8dcf4a3b8c7752
xushiwei <x@goplus.org>
2025-03-01 19:37:48 +0800

    cl test: _testgop/domaintext

--- cl/_testgop/domaintext/out.go
+++ cl/_testgop/domaintext/out.go
@@ -1,19 +1,9 @@
 package main
 
-import (
-	"github.com/goplus/gop/tpl"
-	"github.com/qiniu/x/errors"
-)
+import "github.com/goplus/gop/tpl"
 
 func main() {
-	cl := func() (_gop_ret tpl.Compiler) {
-		var _gop_err error
-		_gop_ret, _gop_err = tpl.New(`expr = INT % ("+" | "-")`)
-		if _gop_err != nil {
-			_gop_err = errors.NewFrame(_gop_err, "tpl`expr = INT % (\"+\" | \"-\")`", "/Users/xushiwei/work/gop/cl/_testgop/domaintext/in.gop", 1, "main.main")
-			panic(_gop_err)
-		}
-		return
-	}()
+	cl, err := tpl.New(`expr = INT % ("+" | "-")`)
 	cl.ParseExpr("1+2", nil)
+	_ = err
 }
